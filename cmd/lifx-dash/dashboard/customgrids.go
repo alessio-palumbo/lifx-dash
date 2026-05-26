006e017e4bfaf1c7c181fde7a7779b3dd71c26d4
alessio-palumbo <alessio.palumbo4@gmail.com>
2026-05-27 08:54:35 +1000

    Update custom grids with latest products

--- cmd/lifx-dash/dashboard/customgrids.go
+++ cmd/lifx-dash/dashboard/customgrids.go
@@ -4,34 +4,42 @@ import "github.com/alessio-palumbo/lifxlan-go/pkg/device"
 
 var candleProducts = map[int]struct{}{
 	57:  {},
+	67:  {},
 	68:  {},
+	81:  {},
+	96:  {},
 	137: {},
 	138: {},
 	185: {},
 	186: {},
+	187: {},
+	188: {},
 	215: {},
 	216: {},
 	217: {},
 	218: {},
 }
 
-var ceilingProducts = map[int]struct{}{
-	176: {},
-	177: {},
-	197: {},
-	198: {},
-	199: {},
-	200: {},
-}
-
-var ceiling13Products = map[int]struct{}{
-	201: {},
-	202: {},
-}
-
 var lunaProducts = map[int]struct{}{
 	219: {},
 	220: {},
+}
+
+var ceilingProducts = map[int]struct{}{
+	// Ceiling 15"
+	145: {},
+	146: {},
+	176: {},
+	177: {},
+
+	// Ceiling 13"
+	265: {},
+	266: {},
+}
+
+var ceilingCapsuleProducts = map[int]struct{}{
+	201: {},
+	202: {},
 }
 
 type GridRules struct {
@@ -46,6 +54,14 @@ func CustomGridRules(d *device.Device) *GridRules {
 			},
 		}
 	}
+	if _, ok := lunaProducts[int(d.ProductID)]; ok {
+		return &GridRules{
+			HiddenIndexes: map[int]bool{
+				0: true, 6: true,
+				28: true, 34: true,
+			},
+		}
+	}
 	if _, ok := ceilingProducts[int(d.ProductID)]; ok {
 		return &GridRules{
 			HiddenIndexes: map[int]bool{
@@ -54,19 +70,11 @@ func CustomGridRules(d *device.Device) *GridRules {
 			},
 		}
 	}
-	if _, ok := ceiling13Products[int(d.ProductID)]; ok {
+	if _, ok := ceilingCapsuleProducts[int(d.ProductID)]; ok {
 		return &GridRules{
 			HiddenIndexes: map[int]bool{
 				0: true, 1: true, 14: true, 15: true,
 				112: true, 113: true, 126: true, 127: true,
-			},
-		}
-	}
-	if _, ok := lunaProducts[int(d.ProductID)]; ok {
-		return &GridRules{
-			HiddenIndexes: map[int]bool{
-				0: true, 6: true,
-				28: true, 34: true,
 			},
 		}
 	}
